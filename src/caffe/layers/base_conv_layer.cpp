3eb2242cdfea6b929b0cf41bbbbaccb9a81342b4
Zhishuai Zhang <zhshuai.zhang@gmail.com>
2017-01-10 15:30:46 -0500

    max_conv bug fixed

--- src/caffe/layers/base_conv_layer.cpp
+++ src/caffe/layers/base_conv_layer.cpp
@@ -279,6 +279,7 @@ void BaseConvolutionLayer<Dtype>::forward_cpu_max_conv(const Dtype* input,
                   weights + this->weight_offset_ * g + im_ * this->blobs_[0]->count(1), 
                   transposed_col_buff_, 
                   dot_proc_);
+                  Dtype max_sum = 0;
               for (int ic_ = 0; ic_ < this->blobs_[0]->shape(1); ++ic_) { // 176 in our case
                   int max_idx = -1;
                   Dtype max_val = -std::numeric_limits<Dtype>::infinity();
@@ -288,12 +289,13 @@ void BaseConvolutionLayer<Dtype>::forward_cpu_max_conv(const Dtype* input,
                           max_idx = max_idx_ - ic_ * this->blobs_[0]->count(2);
                       }
                   }
-                  if (max_val > 0.001) {
-                      LOG(INFO) << "max_val" << max_val;
-                  }
-                  output[in_ + this->conv_out_spatial_dim_ * im_] = max_val;
-                  max_mask[in_ + this->conv_out_spatial_dim_ * (ic_ + this->blobs_[0]->shape(1) * (im_ + this->conv_out_channels_ * num_idx))] = max_idx;
+                  max_sum += max_val;
+                  max_mask[in_ + conv_out_spatial_dim_ * (ic_ + this->blobs_[0]->shape(1) * (im_ + this->conv_out_channels_ * num_idx))] = max_idx;
               }
+              if (max_sum > 0.001) {
+                  LOG(INFO) << "max_sum" << max_sum;
+              }
+              output[in_ + conv_out_spatial_dim_ * im_] = max_sum;
           }
       }
   }
