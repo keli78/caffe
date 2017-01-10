5218383c86f5a6b458a24038ed6dbf1eaf76acc8
Zhishuai Zhang <zhshuai.zhang@gmail.com>
2017-01-10 14:57:10 -0500

    fix pure virtual function for maxconv

--- include/caffe/layers/max_conv_layer.hpp
+++ include/caffe/layers/max_conv_layer.hpp
@@ -68,10 +68,11 @@ class MaxConvolutionLayer : public BaseConvolutionLayer<Dtype> {
   
 
  protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+  void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) {
+  void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down,
+      const vector<Blob<Dtype>*>& bottom) {
           NOT_IMPLEMENTED;
       }
   virtual inline bool reverse_dimensions() { return false; }
