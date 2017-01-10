1162682ea9383112b6e3cbd6a2a25e143bb92fe8
Zhishuai Zhang <zhshuai.zhang@gmail.com>
2017-01-09 20:21:46 -0500

    bug fixed for max_conv

--- src/caffe/layers/max_conv_layer.cpp
+++ src/caffe/layers/max_conv_layer.cpp
@@ -7,11 +7,11 @@ namespace caffe {
 template <typename Dtype>
 void MaxConvolutionLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
           const vector<Blob<Dtype>*>& top) { // TODO(Zhishuai): We assume group = 1 here
-      const int first_spatial_axis = channel_axis_ + 1;
-      CHECK_EQ(bottom[0]->num_axes(), first_spatial_axis + num_spatial_axes_)
+      const int first_spatial_axis = this->channel_axis_ + 1;
+      CHECK_EQ(bottom[0]->num_axes(), first_spatial_axis + this->num_spatial_axes_)
           << "bottom num_axes may not change.";
-      num_ = bottom[0]->count(0, channel_axis_);
-      CHECK_EQ(bottom[0]->shape(channel_axis_), channels_)
+      this->num_ = bottom[0]->count(0, channel_axis_);
+      CHECK_EQ(bottom[0]->shape(channel_axis_), this->channels_)
           << "Input size incompatible with convolution kernel.";
       // TODO: generalize to handle inputs of different shapes.
       for (int bottom_id = 1; bottom_id < bottom.size(); ++bottom_id) {
@@ -23,23 +23,23 @@ void MaxConvolutionLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       compute_output_shape();
       vector<int> top_shape(bottom[0]->shape().begin(),
           bottom[0]->shape().begin() + channel_axis_);
-      top_shape.push_back(num_output_);
-      for (int i = 0; i < num_spatial_axes_; ++i) {
-        top_shape.push_back(output_shape_[i]);
+      top_shape.push_back(this->num_output_);
+      for (int i = 0; i < this->num_spatial_axes_; ++i) {
+        top_shape.push_back(this->output_shape_[i]);
       }
       for (int top_id = 0; top_id < top.size(); ++top_id) {
         top[top_id]->Reshape(top_shape);
       }
       if (reverse_dimensions()) {
-        conv_out_spatial_dim_ = bottom[0]->count(first_spatial_axis);
+        this->conv_out_spatial_dim_ = bottom[0]->count(first_spatial_axis);
       } else {
-        conv_out_spatial_dim_ = top[0]->count(first_spatial_axis);
+        this->conv_out_spatial_dim_ = top[0]->count(first_spatial_axis);
       }
-      col_offset_ = kernel_dim_ * conv_out_spatial_dim_;
-      output_offset_ = conv_out_channels_ * conv_out_spatial_dim_ / group_;
+      this->col_offset_ = this->kernel_dim_ * this->conv_out_spatial_dim_;
+      this->output_offset_ = this->conv_out_channels_ * this->conv_out_spatial_dim_ / this->group_;
       // Setup input dimensions (conv_input_shape_).
-      vector<int> bottom_dim_blob_shape(1, num_spatial_axes_ + 1);
-      conv_input_shape_.Reshape(bottom_dim_blob_shape);
+      vector<int> bottom_dim_blob_shape(1, this->num_spatial_axes_ + 1);
+      this->conv_input_shape_.Reshape(bottom_dim_blob_shape);
       int* conv_input_shape_data = conv_input_shape_.mutable_cpu_data();
       for (int i = 0; i < num_spatial_axes_ + 1; ++i) {
         if (reverse_dimensions()) {
@@ -51,50 +51,33 @@ void MaxConvolutionLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       // The im2col result buffer will only hold one image at a time to avoid
       // overly large memory usage. In the special case of 1x1 convolution
       // it goes lazily unused to save memory.
-      col_buffer_shape_.clear();
-      col_buffer_shape_.push_back(kernel_dim_ * group_);
+      this->col_buffer_shape_.clear();
+      this->col_buffer_shape_.push_back(kernel_dim_ * group_);
       for (int i = 0; i < num_spatial_axes_; ++i) {
         if (reverse_dimensions()) {
-          col_buffer_shape_.push_back(input_shape(i + 1));
+          this->col_buffer_shape_.push_back(this->input_shape(i + 1));
         } else {
-          col_buffer_shape_.push_back(output_shape_[i]);
+          this->col_buffer_shape_.push_back(this->output_shape_[i]);
         }
       }
-      col_buffer_.Reshape(col_buffer_shape_);
-      bottom_dim_ = bottom[0]->count(channel_axis_);
-      top_dim_ = top[0]->count(channel_axis_);
-      num_kernels_im2col_ = conv_in_channels_ * conv_out_spatial_dim_;
-      num_kernels_col2im_ = reverse_dimensions() ? top_dim_ : bottom_dim_;
+      this->col_buffer_.Reshape(this->col_buffer_shape_);
+      this->bottom_dim_ = bottom[0]->count(this->channel_axis_);
+      this->top_dim_ = top[0]->count(this->channel_axis_);
+      this->num_kernels_im2col_ = this->conv_in_channels_ * this->conv_out_spatial_dim_;
+      this->num_kernels_col2im_ = reverse_dimensions() ? this->top_dim_ : this->bottom_dim_;
       // Set up the all ones "bias multiplier" for adding biases by BLAS
-      out_spatial_dim_ = top[0]->count(first_spatial_axis);
+      this->out_spatial_dim_ = top[0]->count(this->first_spatial_axis);
       if (bias_term_) {
-        vector<int> bias_multiplier_shape(1, out_spatial_dim_);
-        bias_multiplier_.Reshape(bias_multiplier_shape);
-        caffe_set(bias_multiplier_.count(), Dtype(1),
-            bias_multiplier_.mutable_cpu_data());
+        vector<int> bias_multiplier_shape(1, this->out_spatial_dim_);
+        this->bias_multiplier_.Reshape(this->bias_multiplier_shape);
+        caffe_set(this->bias_multiplier_.count(), Dtype(1),
+            this->bias_multiplier_.mutable_cpu_data());
       }
       max_idx_.Reshape(bottom[0]->num(), num_output_, channels_, 
         conv_out_spatial_dim_); // BatchSize*39*176*(14*14) in our case
 }
 
 template <typename Dtype> // Copied from conv layer code
-void MaxConvolutionLayer<Dtype>::compute_output_shape() {
-  const int* kernel_shape_data = this->kernel_shape_.cpu_data();
-  const int* stride_data = this->stride_.cpu_data();
-  const int* pad_data = this->pad_.cpu_data();
-  const int* dilation_data = this->dilation_.cpu_data();
-  this->output_shape_.clear();
-  for (int i = 0; i < this->num_spatial_axes_; ++i) {
-    // i + 1 to skip channel axis
-    const int input_dim = this->input_shape(i + 1);
-    const int kernel_extent = dilation_data[i] * (kernel_shape_data[i] - 1) + 1;
-    const int output_dim = (input_dim + 2 * pad_data[i] - kernel_extent)
-        / stride_data[i] + 1;
-    this->output_shape_.push_back(output_dim);
-  }
-}
-
-template <typename Dtype>
 void MaxConvolutionLayer<Dtype>::compute_output_shape() {
   const int* kernel_shape_data = this->kernel_shape_.cpu_data();
   const int* stride_data = this->stride_.cpu_data();
