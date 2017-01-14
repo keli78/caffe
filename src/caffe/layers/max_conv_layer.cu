9d5c88ab72f4bb46111b561fa4fd6b31796b4d44
Zhishuai Zhang <zhshuai.zhang@gmail.com>
2017-01-14 10:44:40 -0800

    bug fixing

--- src/caffe/layers/max_conv_layer.cu
+++ src/caffe/layers/max_conv_layer.cu
@@ -8,15 +8,15 @@ namespace caffe {
 template <typename Dtype>
 void MaxConvolutionLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
-  const Dtype* weight = this->blobs_[0]->cpu_data();
+  const Dtype* weight = this->blobs_[0]->gpu_data();
   for (int i = 0; i < bottom.size(); ++i) {
-    const Dtype* bottom_data = bottom[i]->cpu_data();
-    Dtype* top_data = top[i]->mutable_cpu_data();
+    const Dtype* bottom_data = bottom[i]->gpu_data();
+    Dtype* top_data = top[i]->mutable_gpu_data();
     for (int n = 0; n < this->num_; ++n) {
       this->forward_gpu_max_conv(bottom_data + n * this->bottom_dim_, weight,
           top_data + n * this->top_dim_, n);
       if (this->bias_term_) {
-        const Dtype* bias = this->blobs_[1]->cpu_data();
+        const Dtype* bias = this->blobs_[1]->gpu_data();
         this->forward_gpu_bias(top_data + n * this->top_dim_, bias);
       }
     }
